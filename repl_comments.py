01c4f6092b7ab3972992a27082b0b5154f7a7d35
Yusuf Hilmi <177yusuf177@gmail.com>
2018-12-19 21:16:01 +0300

    Step 3 finished

--- repl_comments.py
+++ repl_comments.py
@@ -1,9 +1,27 @@
+from hashlib import sha256
+
+
+def create_hash(password):              # This function is taken from hash_password.py
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
+
+
 input_list = []
 a = 1
 while True:
     user_input = input("Enter your comment: ")
-    input_list.append(user_input)
-    print("Previously entered comments: ")
-    for x in range(a):
-        print((x+1), ". ", input_list[x], sep="")
-    a += 1
+
+    pw1 = input('Please enter your password:')   # This line is taken from hash_password
+    hsh1 = create_hash(pw1)
+
+    pw_original = "TerriblePassword"             # This line is taken from hash_password.py
+    hsh_original = create_hash(pw_original)      # This line is taken from hash_password.py
+    if hsh1 == hsh_original:
+        input_list.append(user_input)
+        print("Previously entered comments: ")
+        for x in range(a):
+            print((x+1), ". ", input_list[x], sep="")
+        a += 1
+    else:
+        print("I am sorry I can't let you do that.")
+
